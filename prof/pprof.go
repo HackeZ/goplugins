9ccad874c523409d9ce0641cd01aac73f5143cd5
HackeZ <767110505@qq.com>
2018-01-28 22:00:45 +0800

    grammar error fixed

--- prof/pprof.go
+++ prof/pprof.go
@@ -1,13 +1,14 @@
 package prof
 
 import (
-	"time"
-	"os"
-	"fmt"
-	"strings"
 	"bytes"
-	"os/exec"
+	"fmt"
+	"log"
 	_ "net/http/pprof"
+	"os"
+	"os/exec"
+	"strings"
+	"time"
 )
 
 const (
@@ -17,18 +18,19 @@ const (
 var pprofArgs = []string{
 	"tool",
 	"pprof",
-	"-raw"
+	"-raw",
 }
 
 func raw(opts ...string) ([]byte, error) {
 
 	allArgs := make([]string, len(pprofArgs)+len(opts))
-	allArgs = append(allArgs, pprofArgs, opts)
+	allArgs = append(allArgs, pprofArgs...)
+	allArgs = append(allArgs, opts...)
 
-	log.Println("run pprof command: go %s", strings.Join(allArgs, " "))
-	
+	log.Printf("run pprof command: go %s\n", strings.Join(allArgs, " "))
+
 	var buf bytes.Buffer
-	cmd := exec.Command("go", allArgs)
+	cmd := exec.Command("go", allArgs...)
 	cmd.Stderr = &buf
 
 	out, err := cmd.Output()
@@ -39,19 +41,19 @@ func raw(opts ...string) ([]byte, error) {
 }
 
 func save(filepath string, raw []byte) error {
-	f, err := os.Create(filepath+getFileName())
+	f, err := os.Create(filepath + getFileName())
 	if err != nil {
-		return fmt.Errorf("failed to create profile file: ", err.Error())
+		return fmt.Errorf("failed to create profile file: %s", err.Error())
 	}
 
 	_, err = f.Write(raw)
 	if err != nil {
-		return fmt.Errorf("failed to write pprof to file: ", err.Error())
+		return fmt.Errorf("failed to write pprof to file: %s", err.Error())
 	}
 	log.Printf("write pprof raw to file: %s success\n", f.Name())
 	return nil
 }
 
 func getFileName() string {
-	return time.Now().Format("20060102_150405")+fileSuffix
+	return time.Now().Format("20060102_150405") + fileSuffix
 }
